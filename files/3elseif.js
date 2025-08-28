d1f0055daa56ee0ce3c3f976e6b30e4d68e59e49
omkar gundale <omkar.comp.er@gmail.com>
2025-08-28 23:14:56 +0530

    Add files via upload

---
+++ files/3elseif.js
@@ -0,0 +1,18 @@
+let age = 118;
+
+if (age > 18){
+ console.log("you can vote");
+}
+
+else if (age < 18){
+ console.log("you can't vote");
+}
+
+else if (age = 118){
+ console.log("you can't vote");
+}
+
+// you cant use else if first
+// in if else if of the upper statement is true the below statement is not executed
+// in else if either the if will be executed or else and the
+// there can be multiple if and else if 
