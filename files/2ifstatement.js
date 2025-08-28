4d5f533f31131c607828cd68c201f917b0b557bc
omkar gundale <omkar.comp.er@gmail.com>
2025-08-28 23:12:56 +0530

    Add files via upload

---
+++ files/2ifstatement.js
@@ -0,0 +1,14 @@
+console.log( "before my if start");
+let age =18;
+
+if (age > 18){
+ console.log("you can vote");
+ a="wait for next election"
+ console.log(a)
+}
+
+if (age < 18){
+ console.log("you can't vote");
+}
+
+console.log( "after my if start");
