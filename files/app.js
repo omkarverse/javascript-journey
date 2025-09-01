b629e44bd601e2f75bcfad27d8d0d70aa519d96d
omkar gundale <omkar.comp.er@gmail.com>
2025-09-01 19:30:33 +0530

    Add files via upload

---
+++ files/app.js
@@ -0,0 +1,7 @@
+let pensil = 10;
+let eraser = 5;
+
+// let output= "total price:" +(eraser + pensil) + "rupees.";
+// console.log("total price:" ,eraser + pensil, "rupees.");
+let output=`the total price of pensil is ${pensil}`;
+console.log(output);
