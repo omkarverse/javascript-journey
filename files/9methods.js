40017845bebc6563796ea8bf964496aac1493dce
omkar gundale <omkar.comp.er@gmail.com>
2025-09-02 21:48:42 +0530

    Add files via upload

---
+++ files/9methods.js
@@ -0,0 +1,9 @@
+let msg = "    he   llo    "
+console.log(msg);
+msg.trim();
+
+// can be used to remove and trim spaces from 
+// thr password while setting password of anything
+
+// strings are immutable
+// this trim is not parmanent
