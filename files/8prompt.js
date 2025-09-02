75f14a039508c1437b2be221f95fd3c553d6615b
omkar gundale <omkar.comp.er@gmail.com>
2025-09-02 21:48:25 +0530

    Add files via upload

---
+++ files/8prompt.js
@@ -0,0 +1,9 @@
+let firstname = prompt("enter your firstname");
+let lastname = prompt("enter your lastname");
+
+// popup will ask for an input 
+
+
+console.log("welcome" , firstname,"",lastname );
+// this will print it in console
+
