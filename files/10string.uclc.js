edb19366528317129ea25f276cb5ae0af7f8ac20
omkar gundale <omkar.comp.er@gmail.com>
2025-09-03 15:50:08 +0530

    Add files via upload

---
+++ files/10string.uclc.js
@@ -0,0 +1,17 @@
+let name=("     Omkar Gundale    ");
+// name.toLowerCase()
+// name.toupperCase()
+
+// both the above strings only work in console due to some issue
+
+
+// Q. now remove the spaces and convert a string into uppercase
+// let newmassage = name.trim();
+// console.log(newmassage);
+// newmassage = newmassage.toUpperCase();
+// console.log( newmassage); 
+
+// or
+
+let newmassage =name.trim().toUpperCase();
+console.log(newmassage);
