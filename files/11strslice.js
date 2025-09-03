986a794c730be8baeb03458aeb3930cf5b40734c
omkar gundale <omkar.comp.er@gmail.com>
2025-09-03 15:50:38 +0530

    Add files via upload

---
+++ files/11strslice.js
@@ -0,0 +1,17 @@
+let name = ("mromkargundale")
+let newName = name.slice(2);
+// here from 2nd index all string 
+// Elementare displayed till the end
+
+let newName2 = newName.slice(0,5);
+// it will slice the string from 0 to 4th indexe
+// the 5 is not included 
+
+console.log(newName);
+console.log(newName2);
+
+
+
+// let newName = name.slice(-1); 
+// it is as ending index
+// so here str length 14 - 1 hense index 13 th letter will be print
