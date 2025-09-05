e1de47bc43727a3c44624c7cf2637f16ee74f7af
omkar gundale <omkar.comp.er@gmail.com>
2025-09-05 23:42:43 +0530

    Add files via upload

---
+++ files/13array.js
@@ -0,0 +1,13 @@
+let cars = ["audi", "bmw", "mercedese", "rollsroyce"];
+
+let cars1 = cars.push("mahindra");
+
+let cars2 = cars1.pop;
+
+console.log("before push:", cars);
+
+console.log("after push:", cars1);
+
+console.log("after push:", cars2);
+
+
