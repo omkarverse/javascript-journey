05ff969a03668313d57086edc2ee0de9c4f1a4a1
omkar gundale <omkar.comp.er@gmail.com>
2025-09-07 21:27:55 +0530

    Add files via upload

---
+++ files/15array.js
@@ -0,0 +1,7 @@
+let color = ["red", "blue","green", "orange", "purple", "yellow", "white"];
+
+console.log(color.slice());
+console.log(color.slice(2));
+console.log(color.slice(3,4));
+console.log(color.slice(-2));
+console.log(color.slice(-4));
