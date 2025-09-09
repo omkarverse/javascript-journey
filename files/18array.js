ef9173961f542993f3814502ae082324cd725225
omkar gundale <omkar.comp.er@gmail.com>
2025-09-09 15:12:17 +0530

    Add files via upload

---
+++ files/18array.js
@@ -0,0 +1,7 @@
+// find index of javascript after reversing the index
+
+let lang = ["c++", "java", "go", "typescript", "javascript", "python", "css"];
+
+let newlang = lang.reverse();
+
+console.log(newlang.indexOf("javascript"));  // Output: 2
