245c4dc64ebfd7b29318942e0ae6da0f072aa753
omkar gundale <omkar.comp.er@gmail.com>
2025-09-11 23:14:09 +0530

    Add files via upload

---
+++ files/19const.js
@@ -0,0 +1,7 @@
+const lang = ["c++", "java", "go", "typescript", "javascript", "python", "css"];
+
+// the const variable cannot be changed into
+// new array
+// only method is used to change it little bit
+
+console.log(lang.push("LLM"));
