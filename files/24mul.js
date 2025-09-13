aadec317c7402a8eb8ab08f9035097a544db992e
omkar gundale <omkar.comp.er@gmail.com>
2025-09-13 17:07:07 +0530

    Add files via upload

---
+++ files/24mul.js
@@ -0,0 +1,20 @@
+// // multiple of 5
+for (i=5; i<=50; i=i+5){
+    console.log(i);
+}
+
+
+// // multiple of 3
+for (i=3; i<=30; i=i+3){
+    console.log(i);
+}
+
+
+// take input from user
+let n = prompt(" enter number ");
+n = parseInt(n);
+
+for (i=n; i<=n*10; i=i+n){
+    console.log(i);
+}
+
