088248f1628082a89fe375b40f172865f259c354
omkar gundale <omkar.comp.er@gmail.com>
2025-09-16 12:57:14 +0530

    Add files via upload

---
+++ files/27forof.js
@@ -0,0 +1,11 @@
+const lang = ["c++", "java", "go", "typescript", "javascript", "python", "css"];
+
+for( languages of lang){
+    console.log(languages);
+}
+
+// the lang elements will be stored in languages one by one
+
+for(char of "apnacollage"){
+    console.log(char);
+}
