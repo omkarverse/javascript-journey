92239e3795a2fedde36d6cd5eaf03f495fc8e46a
omkar gundale <omkar.comp.er@gmail.com>
2025-09-17 14:28:54 +0530

    Add files via upload

---
+++ files/29.js
@@ -0,0 +1,11 @@
+let obj = {
+  1: "a",
+  2: "b",
+  3: "null",
+}
+
+console.log(obj[1]);
+
+// obj[1] is used because object stores the keys 
+// as in the value of string and then it matches it with 
+// the obj keys
