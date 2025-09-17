027b36ebd7e3a8fee3ee834873367dca3d29077a
omkar gundale <omkar.comp.er@gmail.com>
2025-09-17 14:29:13 +0530

    Add files via upload

---
+++ files/30math.js
@@ -0,0 +1,21 @@
+
+console.log(Math.PI)
+console.log(Math.E)//eular value
+
+
+
+console.log(Math.abs(12))//positive to positive 
+console.log(Math.abs(-22))//convert the number into positive
+console.log(Math.pow(2,4))//2 to the power 4
+console.log(Math.pow(2,5))
+
+console.log(Math.floor(5.5))//round of nearest smallest int value
+console.log(Math.floor(5))
+console.log(Math.floor(-5.4))
+
+console.log(Math.ceil(5.5))//round of nearest largest int value
+console.log(Math.ceil(5))
+console.log(Math.ceil(-5.4))
+console.log(Math.ceil(5.000000001))
+
+console.log(Math.random())//0 to 1 any random value except 1
