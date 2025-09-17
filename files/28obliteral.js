7ee27b840a112592b7b9b01ab12f67f544d1760e
omkar gundale <omkar.comp.er@gmail.com>
2025-09-17 14:28:33 +0530

    Add files via upload

---
+++ files/28obliteral.js
@@ -0,0 +1,12 @@
+const student = {
+    name: "omkar",
+    age: 22,
+    marks: 7.5
+    
+
+};
+
+// key value pair is called as Property
+// multiple property form Object
+// const student in this student adderess is permenent 
+// but the key value pair can be changed
