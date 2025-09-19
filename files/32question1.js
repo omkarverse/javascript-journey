440bfdff125265a9fe402a3200b89e0662caaf4a
omkar gundale <omkar.comp.er@gmail.com>
2025-09-19 22:24:10 +0530

    Add files via upload

---
+++ files/32question1.js
@@ -0,0 +1,10 @@
+// Qs1.Create a program that generats a random number representing a dice roll.
+// [The number should be between 1 and 6]
+
+// Qs1. Create a program that generates a random number representing a dice roll.
+// [The number should be between 1 and 6]
+
+let dice = Math.floor(Math.random() * 6) + 1;  // gives 1 to 6
+console.log("Dice roll:", dice);
+
+
