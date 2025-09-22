e352dce3033af9ca541cbccd2fd0cd1abba03e20
omkar gundale <omkar.comp.er@gmail.com>
2025-09-22 20:04:28 +0530

    Add files via upload

---
+++ files/36tablefun.js
@@ -0,0 +1,11 @@
+// // create a table that prints the multiplication 
+// // table of a Number
+
+function table(n){
+    for(i=1 ; i<= 10; i++){
+        console.log((n*i));
+        // break;
+    }
+}
+
+table(6);
