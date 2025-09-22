366ce32bf50c0d19cb034b2638edfcfeb13fcfac
omkar gundale <omkar.comp.er@gmail.com>
2025-09-22 20:04:50 +0530

    Add files via upload

---
+++ files/37dun.js
@@ -0,0 +1,18 @@
+// function sum(a, b){
+//     console.log((a+b));
+// }
+
+// sum(4,5);
+// sum(sum(1,2), 3);
+
+
+
+function sum(a, b){
+    return a+b ;
+
+    //anything written after return keyword will 
+    // be ignored
+}
+
+console.log(sum(4,5));
+console.log(sum(sum(1,2), 3));
