b4ca985b5a4af7c72ab1126112a2aa01957d04f4
omkar gundale <omkar.comp.er@gmail.com>
2025-09-25 18:49:09 +0530

    Add files via upload

---
+++ files/44arrow.js
@@ -0,0 +1,18 @@
+let num = (a,b) =>{
+    return a+b
+}
+
+let cube = a =>{   //without parenthesis for single argument
+    return a*a*a
+}
+
+let power = (a,b) =>{
+    return a**b;
+}
+
+
+let hum = (a,b) =>a+b
+//if arrow function gives one value
+//then we can remove the return keyword
+//we can even remove the parenthesis
+
