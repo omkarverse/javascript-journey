fd6b06e4cacdf6fd149b3c962c5ad48c067fe673
omkar gundale <omkar.comp.er@gmail.com>
2025-09-25 18:47:08 +0530

    Add files via upload

---
+++ files/41scope.js
@@ -0,0 +1,11 @@
+function highvaluescope(func, count){
+    for(let i = 1; i<=count; i++){
+        func()
+    }
+}
+
+let greet = function(){
+    console.log("hello");
+}
+
+highvaluescope(greet, 100);
