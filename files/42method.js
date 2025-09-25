820392b563efbcc2cbc68b62f65b434b47aa1f40
omkar gundale <omkar.comp.er@gmail.com>
2025-09-25 18:47:25 +0530

    Add files via upload

---
+++ files/42method.js
@@ -0,0 +1,13 @@
+const calculator = {
+    add: function(a, b) {
+        return a + b;
+    },
+    sub: function(a, b) {
+        return a - b;
+    },
+    mul: function(a, b) {
+        return a * b;
+    }
+};
+
+//everything in javascript is basically an object
