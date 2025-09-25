eeda9121fca4a7fd21509d80d2f8d10d0a1f7045
omkar gundale <omkar.comp.er@gmail.com>
2025-09-25 18:47:46 +0530

    Add files via upload

---
+++ files/43trycatch.js
@@ -0,0 +1,13 @@
+console.log("hello");
+console.log("hello");
+try{
+    console.log(s)//if there is error in try block then 
+    //catch is executed ignoring the error
+}
+catch{
+    console.log("catch an error..a is undefined")
+}//if catch has error then no solution
+
+console.log("hello");
+console.log("hello");
+console.log("hello");
