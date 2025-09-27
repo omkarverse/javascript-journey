c6798834de4b56a46215a55eaea47237a875e05f
omkar gundale <omkar.comp.er@gmail.com>
2025-09-27 23:12:40 +0530

    Update 44arrow.js

--- files/44arrow.js
+++ files/44arrow.js
@@ -2,7 +2,7 @@ let num = (a,b) =>{
     return a+b
 }
 
-let cube = a =>{   //without parenthesis for single argument
+let cube = a =>{   //you can ir cannot use parenthesis for single argument
     return a*a*a
 }
 
@@ -15,4 +15,5 @@ let hum = (a,b) =>a+b
 //if arrow function gives one value
 //then we can remove the return keyword
 //we can even remove the parenthesis
+
 
