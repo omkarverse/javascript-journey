10ffbcc3e210dd949462bef3ecc466808d073381
omkar gundale <omkar.comp.er@gmail.com>
2025-09-29 15:45:19 +0530

    Add files via upload

---
+++ files/45dom.js
@@ -0,0 +1,20 @@
+console.log(window);
+//window object is inbuild in javascript and it is 
+//automatically called
+
+console.log(document);
+//document is also an object which is called 
+//automatically when we call window
+//console.log(window);but this only allows to 
+//access of limited part of document object
+
+console.dir(document);
+//console .dir is an inbuilt function 
+//this func give us propper access of document 
+//document is basically an html code
+
+
+//whenever we want to manupulate the the css and 
+// & html property then we will use console.dir(document);
+
+
