53f7e73edcb47c05e9ace3a1e5f541e37d3a2d39
omkar gundale <omkar.comp.er@gmail.com>
2025-09-30 14:44:34 +0530

    Update app1.js

--- files/extra/app1.js
+++ files/extra/app1.js
@@ -1,1 +1,7 @@
+// change the h2 to peter parker only using javascript
+
+console.dir(document.all);
+console.dir(document.all[8]);
+console.dir(document.all[8].innerText);
+console.dir(document.all[8].innerText="peter parker"); //step by step to change the innertect of h2
 
