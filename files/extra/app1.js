7a6278c3082737013614cbf064c3a5dd1cb4ff80
omkar gundale <omkar.comp.er@gmail.com>
2025-09-30 14:45:38 +0530

    Update app1.js

--- files/extra/app1.js
+++ files/extra/app1.js
@@ -5,3 +5,10 @@ console.dir(document.all[8]);
 console.dir(document.all[8].innerText);
 console.dir(document.all[8].innerText="peter parker"); //step by step to change the innertect of h2
 
+console.dir(document.getElementById("mainImg"));
+let imgobj= document.getElementById("mainImg");
+
+console.log(imgobj);//gives the src link of an image
+console.dir(imgobj);//we can manupulate the image
+
+
