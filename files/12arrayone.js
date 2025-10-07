2658a0dd8f2622110a37e10f01aa69da3e4f7288
omkar gundale <omkar.comp.er@gmail.com>
2025-10-07 23:59:23 +0530

    Update 12arrayone.js

--- files/12arrayone.js
+++ files/12arrayone.js
@@ -1,6 +1,6 @@
 let students = ["omkar", "ketan", "pratik", "abhishek"];
 
-// array has length displayed in console which is inbuild
+// array has length displayed in console which is inbuild (we dont have to do it)
 
 console.log(students[0]);
 // display array index 0 element
@@ -8,5 +8,6 @@ console.log(students[0]);
 
 let info = ["omkar", 22, 8.5]; //mixed array
 // in c++ and java an array is not mixed its only possible in javascript
+
 
 console.log(students.length);// display length
