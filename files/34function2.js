08c25ee78ae7d4ac9a616bf9ddee7cbd0e6ad52a
omkar gundale <omkar.comp.er@gmail.com>
2025-10-08 00:01:23 +0530

    Update 34function2.js

--- files/34function2.js
+++ files/34function2.js
@@ -1,8 +1,10 @@
 // function for a dice 
-
+//this program put random number in the dice from 1 to 6 
+//because we hade used random math function which is inbuld in javascript
 function dice(){
     console.log(Math.floor(Math.random()*6)+1)
 }
 
 dice();
+
 dice();
